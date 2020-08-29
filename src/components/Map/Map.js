c6425cb6a73d66045bbd8a325d77347b1f1fe808
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-08-29 08:24:04 +0100

    Save map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,27 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import L from "leaflet";
 
 const Container = styled.div`
   width: 100%;
+
+  .marker-icon {
+    text-align: center;
+    padding-top: 2px;
+    font-weight: bold;
+    background-color: ${({ theme }) => theme.colors.dark};
+    color: ${({ theme }) => theme.colors.white};
+    border-radius: 12px;
+  }
 `;
 
+const MarkerIcon = (waypointCount) =>
+  new L.divIcon({
+    className: "marker-icon",
+    iconSize: [25, 25],
+    html: waypointCount,
+  });
+
 const Map = () => {
-  const mapRef = React.useRef(null);
+  let mapContainer;
+  const [waypoints, addWaypoint] = useState([]);
 
   useEffect(() => {
-    mapRef.current = L.map("map", {
-      minZoom: 12,
+    const map = L.map(mapContainer, {
       layers: [
         L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
           attribution:
             '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         }),
       ],
-    }).locate({ setView: true, maxZoom: 18 });
-  }, []);
+    }).locate({ setView: true, maxZoom: 16 });
 
-  return <Container id="map"></Container>;
+    map.on("click", (e) => {
+      const marker = L.marker(e.latlng, {
+        icon: MarkerIcon(1),
+        draggable: true,
+      }).addTo(map);
+
+      addWaypoint((prevState) => [
+        ...prevState,
+        {
+          id: 1,
+          coordinates: marker.getLatLng(),
+        },
+      ]);
+    });
+  }, [mapContainer]);
+  console.log(waypoints);
+  return <Container ref={(el) => (mapContainer = el)}></Container>;
 };
 
 export default Map;
